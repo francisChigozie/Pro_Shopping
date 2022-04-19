38c82a57acb73ce1e7aa0b0e73d98413170a8096
chigozieFrancis <francisc@hs-pforzheim.de>
2022-04-19 09:05:03 +0200

    new devop

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,6 +11,7 @@ router.get(
     '/', 
     asyncHandler(async (req, res) => {
     const products = await Product.find({})
+    //throw new Error('Some error')
     res.json(products)
 }))
 
@@ -25,9 +26,10 @@ router.get(
     if(product) {
         res.json(product)
     }else {
-        res.status(404).json({ message: 'Product not found'})
+        res.status(404)
+        throw new Error('Product not found')
     }
-    
-}))
+  })
+)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFond, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -11,10 +12,14 @@ connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+    res.send('API is running...')    
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFond)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
